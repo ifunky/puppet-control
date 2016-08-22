3aeaaa77be804d263b01aa81c7ee1dea78b1efce
Dan <dan@ifunky.net>
2016-08-22 15:15:59 +0100

    Adding default manifest

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,30 @@
 node default {
   notify { 'Im a windows 2012 server!!': }
+
+
+  ini_setting { 'pluginsync':
+    ensure   => present,
+    path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
+    section  => 'main',
+    setting  => 'pluginsync',
+    value    => 'true',
+  }
+
+  ini_setting { 'report':
+    ensure   => present,
+    path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
+    section  => 'main',
+    setting  => 'report',
+    value    => 'true',
+  }
+
+  ini_setting { 'autoflush':
+    ensure   => present,
+    path     => 'C:/ProgramData/PuppetLabs/puppet/etc/puppet.conf',
+    section  => 'main',
+    setting  => 'autoflush',
+    value    => 'true',
+  }
+
+
 }
