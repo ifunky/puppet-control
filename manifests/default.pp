61e09909f62c00eb0b8628af75ae0a3bc51d6215
Dan Gibbons <dan.gibbons@rbi.co.uk>
2017-03-13 14:42:39 +0000

    Use lookup instead of deprecated include

--- manifests/default.pp
+++ manifests/default.pp
@@ -3,7 +3,7 @@ node default {
   $role = $::role
 
   # Always include base classes defined in hiera
-  hiera_include('classes')
+  lookup('classes', Array[String], 'unique').include
 
   # Optionally include role defined on node (typically used in a non ENC environment)
   if (!empty($role)){
