aefe631923dfff76decf8d00b4f58c45e7c4b31f
Dan Gibbons <dan.gibbons@rbi.co.uk>
2017-05-23 09:55:22 +0100

    Fixing bug in choco where it produces -version instead of --version which causes issues

--- site/profile/manifests/windows/software/chrome.pp
+++ site/profile/manifests/windows/software/chrome.pp
@@ -1,13 +1,16 @@
 # Installs Chrome using Chocolatey
 #
+# NOTE: There is a bug in the Chocolatey module that p;roduces -version instead of --version
+#
 # @author Dan
 class profile::windows::software::chrome (
-  $version = '58.0.3029.110'
+  $version = '56.0.2924.76'
 )
 {
 
   package { 'googlechrome':
-    ensure          => $version,
+    #ensure          => $version,
+    install_options => ["--version",$version,'--ignore-checksums'],
     provider        => 'chocolatey',
   }
 }
