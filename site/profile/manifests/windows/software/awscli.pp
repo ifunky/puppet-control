eac35ea9ee37beaa966578d41e2be4283a1fbc70
Dan <dan@ifunky.net>
2017-06-02 17:46:58 +0100

    Adding default region to profile

--- site/profile/manifests/windows/software/awscli.pp
+++ site/profile/manifests/windows/software/awscli.pp
@@ -1,10 +1,18 @@
 # Install AWS CLI tools using Chocolatey
 #
 # @author Dan
-class profile::windows::software::awscli ()
+class profile::windows::software::awscli (
+  $default_region = 'eu-west-1'
+)
 {
 
   package { 'awscli':
     provider        => 'chocolatey',
+  }
+
+  windows_env { 'AWS_DEFAULT_REGION':
+    ensure    => 'present',
+    value     => $default_region,
+    mergemode => clobber,
   }
 }
